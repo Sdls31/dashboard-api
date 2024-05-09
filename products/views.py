11697631aec447dcdf4014a85fc45075af8141f9
Sdls31 <sdls31@gmail.com>
2024-05-09 00:17:39 -0600

    changes

--- products/views.py
+++ products/views.py
@@ -59,8 +59,8 @@ def create_client(request):
 @csrf_exempt
 def create_order(request):
     if request.method == 'POST':
-        client_id = request.POST.get('client_id')
-        product_id = request.POST.get('product_id') 
+        client_id = int(request.POST.get('client_id'))
+        product_id = int(request.POST.get('product_id'))
         quantity = request.POST.get('quantity') 
         details = request.POST.get('details') 
         address = request.POST.get('address')
